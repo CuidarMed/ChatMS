30a6e7ee930a42c2befed8cb662add50a5779e5e
cgonzalo12 <c.gonzalo12@gmail.com>
2025-11-18 15:41:22 -0300

    postCreateChatRoom

---
+++ ChatMS/Controllers/ChatController.cs
@@ -0,0 +1,46 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly ICreateRoomService createRoomService;
+        private readonly IGetChatRoomService getChatRoomService;
+        private readonly IGetUserChatRoomsService getUserChatRoomsService;
+        private readonly IGetChatMessagesService getChatMessagesService;
+        private readonly ISendMessageService sendMessageService;
+        private readonly IMarkMessagesAsReadService markMessagesAsReadService;
+
+        public ChatController(ICreateRoomService createRoomService,IGetChatRoomService getChatRoomService,
+            IGetUserChatRoomsService getUserChatRoomsService,IGetChatMessagesService getChatMessagesService,
+            ISendMessageService sendMessageService,IMarkMessagesAsReadService markMessagesAsReadService)
+        {
+            this.createRoomService = createRoomService;
+            this.getChatRoomService = getChatRoomService;
+            this.getUserChatRoomsService = getUserChatRoomsService;
+            this.getChatMessagesService = getChatMessagesService;
+            this.sendMessageService = sendMessageService;
+            this.markMessagesAsReadService = markMessagesAsReadService;
+        }
+
+        [HttpPost("create/room")]
+        [ProducesResponseType(typeof(ChatRoomRequest), StatusCodes.Status200OK)]
+        public async Task<IActionResult> CreateChatRoom([FromBody] CreateChatRoomRequest request)
+        {
+            try
+            {
+                var chatRoom = await createRoomService.CreateChatRoomAsync(request);
+                return Ok(chatRoom);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+    }
+}
